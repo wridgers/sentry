2c06be58ee3eff0ccf82890e89e934642d415426
Matt Robenolt <matt@ydekproductions.com>
2017-02-17 15:55:11 -0800

    ui: move "blacklisted" stats into Inbound Data Filters page

--- src/sentry/api/endpoints/project_details.py
+++ src/sentry/api/endpoints/project_details.py
@@ -144,6 +144,7 @@ class ProjectDetailsEndpoint(ProjectEndpoint):
             'sentry:csp_ignored_sources_defaults': bool(project.get_option('sentry:csp_ignored_sources_defaults', True)),
             'sentry:csp_ignored_sources': '\n'.join(project.get_option('sentry:csp_ignored_sources', []) or []),
             'sentry:default_environment': project.get_option('sentry:default_environment'),
+            'filters:blacklisted_ips': '\n'.join(project.get_option('sentry:blacklisted_ips', [])),
             'feedback:branding': project.get_option('feedback:branding', '1') == '1',
         }
         data['plugins'] = serialize([
@@ -284,6 +285,10 @@ class ProjectDetailsEndpoint(ProjectEndpoint):
                     clean_newline_inputs(options['sentry:csp_ignored_sources']))
             if 'feedback:branding' in options:
                 project.update_option('feedback:branding', '1' if options['feedback:branding'] else '0')
+            if 'filters:blacklisted_ips' in options:
+                project.update_option(
+                    'sentry:blacklisted_ips',
+                    clean_newline_inputs(options['filters:blacklisted_ips']))
 
             self.create_audit_entry(
                 request=request,
